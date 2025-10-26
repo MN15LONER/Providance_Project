8db3ccc34cc413a9f81c84f24b1604bb8c834378
MN15LONER <mfunekondeya83@gmail.com>
2025-10-26 20:31:34 +0200

    fixed discussions page routing and added indexing on firestore database

--- lib/features/discussions/presentation/pages/new_discussion_page.dart
+++ lib/features/discussions/presentation/pages/new_discussion_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../../../core/theme/app_colors.dart';
+import '../../../../core/constants/route_constants.dart';
 import '../providers/discussions_provider.dart';
 import '../../../auth/presentation/providers/auth_provider.dart';
 
